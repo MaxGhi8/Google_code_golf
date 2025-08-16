a806b7dd8a87cab9180f3d0a5759cd4c4c4904f4
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-08-16 13:07:56 +0200

    save 29 bytes in 260

--- task260.py
+++ task260.py
@@ -1,11 +1,7 @@
 def p(g,E=enumerate):
- k,*_=set(sum(g,[]))-{0,5};n=len(g)
- L=[(i,n-j-1)for i,r in E(g)for j,v in E(r[::-1])if v==5and not r.__setitem__(n-j-1,0)];p,q,r,s=L[0]+L[-1]
- M=[r[:] for r in g]
- if k in g[p][:q]:
-  d=min(q+1,p-1);p-=d+1;q-=d-1
-  while(p<n)*(q<n):M[p][q]=k;p+=1;q+=1
- if k in g[r][s:]:
-  d=min(r+1,s-1);r-=d-1;s-=d+1
-  while(r<n)*(s<n):M[r][s]=k;r+=1;s+=1
+ n=len(g);L=[(i,n-j-1)for i,r in E(g)for j,v in E(r[::-1])if v==5and not r.__setitem__(n-j-1,0)];p,q,r,s=L[0]+L[-1];k,*_=set(sum(g,[]))-{0};M=[r[:] for r in g]
+ v=k in g[p][:q];d=min(q+1,p-1);p-=d+1;q-=d-1
+ while(p<n)*(q<n):M[p][q]=k*v;p+=1;q+=1
+ v=k in g[r][s:];d=min(r+1,s-1);r-=d-1;s-=d+1
+ while(r<n)*(s<n):M[r][s]=k*v;r+=1;s+=1
  return M
