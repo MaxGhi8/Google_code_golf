41a031545beccf0036f655ba075cf67b65a79e90
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-08-17 08:51:44 +0200

    Improve 097 (-45 Bytes)

--- task097.py
+++ task097.py
@@ -1,1 +1,1 @@
-p=lambda g,E=enumerate:[[0 if g[i][j]>0 and all((x==y==0 or not(0<=i+x<len(g)and 0<=j+y<len(r)and g[i+x][j+y]>0))for x in(-1,0,1)for y in(-1,0,1))else g[i][j]for j,c in E(r)]for i,r in E(g)]
+p=lambda g,E=enumerate:[[c*(sum(g[i+x][j+y]for x in(-1,0,1)for y in(-1,0,1)if 0<=i+x<len(g)and 0<=j+y<len(r))!=c)for j,c in E(r)]for i,r in E(g)]
