11c7f70df5a2812fffc0d62406d8aba0a8d006bb
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-08-17 16:28:15 +0200

    improve 362 (-2 Byte)

--- task362.py
+++ task362.py
@@ -1,1 +1,1 @@
-def p(m):k=sum(r[-1]==5for r in m);m=[[x*(x!=5)for x in r]for r in m];return[r[k:]+r[:k]for r in m[-k:]+m[:-k]]
+def p(m):k=sum(5in r for r in m);m=[[x*(x!=5)for x in r]for r in m];return[r[k:]+r[:k]for r in m[-k:]+m[:-k]]
