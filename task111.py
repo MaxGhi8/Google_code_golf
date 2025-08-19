e136977a015e5281059fcb339ae8d44fd269052e
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-08-19 07:49:43 +0200

    improve 111 (-21 Bytes)

--- task111.py
+++ task111.py
@@ -1,1 +1,1 @@
-def p(g,E=enumerate):d=[[g[a+i-1][b+j-1]for a,r in E(g)for b,v in E(r)if v==5 for j in(0,1,2)]for i in(2,3,4)];return d
+p=lambda g,E=enumerate:[[g[i+k][j-1:j+2]for k in(1,2,3)]for i,r in E(g)for j,x in E(r)if x==5][0]
