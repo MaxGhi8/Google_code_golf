fe275106cfcf69a86e5693e5cbc837d769d5f0f8
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-08-23 17:41:41 +0200

    improve 206 (-13 Byte GG Claude)

--- task206.py
+++ task206.py
@@ -1,4 +1,4 @@
-def p(g,f=lambda d:[[*r]for r in zip(*d)if any(r)]):
+def p(g,f=lambda d:[*filter(any,zip(*d))]):
  x,y=divmod(sum(g,[]).index(5),len(g[0]));g[x][y]=0;m=f(f(g))
- for i in(-1,0,1):g[x+i][y-1:y+2]=m[i+1][:]
+ for i in-1,0,1:g[x+i][y-1:y+2]=m[i+1]
  return g
