9eda517c54d7681e038ac4ad81ca7b0812b9bcdb
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-08-29 17:35:04 +0200

    add 058

---
+++ task058.py
@@ -0,0 +1,4 @@
+def p(g):
+ i=j=r=0;X,Y=[0,1,0,-1],[1,0,-1,0]
+ while r<99:g[i][j]=3;x=X[r%4];y=Y[r%4];I=i+x;J=j+y;i,j,r=[(i,j,r+1),(I,J,r)][r<3<len(g)>I>=0<=J<len(g)or r>2and g[I+x][J+y]<1]
+ return g
