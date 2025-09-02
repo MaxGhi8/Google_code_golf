ebe7546f001675701d6ed951db353a29e9a2cf72
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-03 00:13:18 +0200

    improvements 35,64

--- task035.py
+++ task035.py
@@ -1,5 +1,5 @@
 def p(g,e=enumerate):
  (a,b),*_,(c,d)=[(i,j)for i,r in e(g)for j,v in e(r)if v==8]
- for r in g[a:c+1]:r[b]=r[0]or r[b];r[d]=r[9]or r[d]
- for j in range(b,d+1):g[a][j]=g[0][j]or g[a][j];r[j]=g[9][j]or r[j]
+ for r in g[a:c+1]:r[b],r[d]=r[0]or r[b],r[9]or r[d]
+ while d>=b:g[a][d],r[d]=g[0][d]or g[a][d],g[9][d]or r[d];d-=1
  return g
