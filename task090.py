096fa6dae8de00058696f1d78107afce36fbdf71
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-03 23:31:23 +0200

    90 -153

--- task090.py
+++ task090.py
@@ -1,17 +1,11 @@
-def p(g,R=range,e=enumerate):
- h=[0]*(W:=len(g[m:=0]))
- for i,r in e(g):
-  for j,v in e(r):h[j]=[0,h[j]+1][v<1]
-  L,S=[-1]*W,[W]*W;s=[]
-  for j,v in e(h):
-   while s and h[s[-1]]>=v:s.pop()
-   L[j]=s[-1]if s else-1;s+=j,
-  s=[]
-  for j,v in list(e(h))[::-1]:
-   while s and h[s[-1]]>v:s.pop()
-   S[j]=s[-1]if s else W;s+=j,
-  for K,A,F in zip(h,L,S):
-   if K>1<(a:=K*(F+~A))>m:m,Y,X=a,(-K-~i,-~i),(-~A,F)
- for y in R(*Y):
-  for x in R(*X):g[y][x]=6
+def p(g,r=range,e=enumerate):
+ h=[0]*(W:=-~len(g[m:=0]))
+ for i,R in e(g):
+  for j,v in e(R):h[j]=v<1and h[j]+1;s=[-1]
+  for j in r(W):
+   while-~s[-1]and h[s[-1]]>=h[j]:
+    K=h[s.pop()];a=K*(j+~s[-1])
+    if 1<K<a>m:m,A,B,C,D=a,-K-~i,-~i,-~s[-1],j
+   s+=j,
+ for y in r(A,B):g[y][C:D]=[6]*(D-C)
  return g
