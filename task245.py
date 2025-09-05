ddd04bac365560f92b9a1c15a220b8e18ddabf0e
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-05 20:24:39 +0200

    245 -5

--- task245.py
+++ task245.py
@@ -1,4 +1,4 @@
 def p(g,f=lambda g:[[v%3for v in r]for r in zip(*g)if 2in r]):
- n=[[v-v%3for v in r]for r in g];a,b=divmod(sum(g,[]).index(3),len(g[0]))
- for x in range(5):n[a+1+x][b+1:b+1+5]=f(f(g))[x]
+ n=[[v-v%3for v in r]for r in g];t=sum(g,[]).index(3);w=len(g[0])
+ for x in range(5):n[t//w+1+x][t%w+1:t%w+6]=f(f(g))[x]
  return n
