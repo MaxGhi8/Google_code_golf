b3c2f06f703460ecdd9111ef699a9cb785d14a38
alessdal <alessandro.daldossi@gmail.com>
2025-09-05 12:56:26 +0200

    182 -6 B + new tip

--- task182.py
+++ task182.py
@@ -2,6 +2,5 @@ def p(g,R=range):
  z=max(z+21for z in R(274)if(f:=sum(g,[]))[z]==5==f[z+126]);l=max(t:=sum(s:=[r[z%20:][:5]for r in g[z//20:][:5]],[]))
  for i in R(256):
   y=i>>4;x=i&15
-  if all(g[y+a//5][x+a%5]*l==t[a]for a in R(25)):
-   for a in R(5):g[y+a][x:x+5]=s[a]
+  for a in[*R(5)]*all(g[y+a//5][x+a%5]*l==t[a]for a in R(25)):g[y+a][x:x+5]=s[a]
  return g
