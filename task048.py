4aa241bc756334b1b9c29f0bb32019c8a3be0687
alessdal <alessandro.daldossi@gmail.com>
2025-09-10 09:08:42 +0200

    add 48

---
+++ task048.py
@@ -0,0 +1,10 @@
+def p(g,R=range):
+ h=len(g);w=len(g[0])
+ s=[(i,j)for i in R(h-1)for j in R(w-1)if g[i][j]*g[i+1][j+1]==4]
+ f=lambda i,j:[(r,c)for r in R(i-1,i+3)for c in R(j-1,j+3)if h>r>-1<c<w and g[r][c]>7];q=f(*s[0])
+ while q:
+  r,c=q.pop()
+  if(r,c)in f(*s[1]):return[[8]]
+  g[r][c]=0
+  for a,b in(1,0),(-1,0),(0,1),(0,-1):x=r+a;y=c+b;h>x>-1<y<w and g[x][y]>7and q.append((x,y))
+ return[[0]]
