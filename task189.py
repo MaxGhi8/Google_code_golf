bca35d02d5edc0379b66001a549e5b490d462192
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-10 22:43:10 +0200

    189 -20

--- task189.py
+++ task189.py
@@ -1,3 +1,3 @@
-def p(g,k=0,R=range(3,9)):
- if g[0][2]*g[2][0]==64:g=[[g[(r-2)//4][(c-2)//4]*(g[r][c]>0)for c in R]for r in R]
- return(k>3)*g or p([*map(list,zip(*g[::-1]))],k+1)
+def p(g,k=0,a=range(3,9)):
+ if g[0][2]*g[2][0]==64:g=[[g[r>5][c>5]*(g[r][c]>0)for c in a]for r in a]
+ return(k>3)*g or p([*zip(*g[::-1])],k+1)
