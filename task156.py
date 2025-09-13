acb0d0ce6c6aa88df6f57a709e65a7e246752630
osea <osea.fracchia@iusspavia.it>
2025-09-13 20:32:24 +0200

    156 -2

--- task156.py
+++ task156.py
@@ -1,6 +1,6 @@
 def p(g,R=range,N=2):
- for f,a,b,c,d in sorted((-all(all(r[b:b+h])for r in g[a:a+k])*k*h,a,b,a+k,b+h)for a in R(9)for b in R(9)for k in R(11-a)for h in R(11-b)):
+ for _,a,b,c,d in sorted((-k*h,a,b,a+k,b+h)for a in R(9)for b in R(9)for k in R(11-a)for h in R(11-b) if all(all(r[b:b+h])for r in g[a:a+k])):
   for i in R(a+1,c-1):
-   for j in[*R(b+1,d-1)]*-f:g[i][j]-=(g[i][j]>2)*N
+   for j in R(b+1,d-1):g[i][j]-=(g[i][j]>2)*N
   N=3
  return g
