329c41decb7d4c0b8c2e2d461caaffe369b51909
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-13 21:01:39 +0200

    202 -6

--- task202.py
+++ task202.py
@@ -1,6 +1,6 @@
 def p(g,e=enumerate):
- for _ in'  ':
+ for _ in 0,0:
   g=[*map(list,zip(*g))]
-  for i,r in e(g*(len({*g[0]}-{0})>1)):
-   for j,v in e(r):r[j]=[v,0][0 in r and v in{x[r.index(0)]for x in g[i-3:]}]
+  for i,r in e(g*(len({*g[0],0})>2)):
+   for j,v in e(r):r[j]=[v,0][0in r and v in[*zip(*g[i-3:])][r.index(0)]]
  return g
