f3034a34f753e7eb64919fad556fe9aed799b4fe
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-13 20:46:39 +0200

    156 -4

--- task156.py
+++ task156.py
@@ -1,6 +1,6 @@
-def p(g,R=range,N=2):
- for _,a,b,c,d in sorted((-k*h,a,b,a+k,b+h)for a in R(9)for b in R(9)for k in R(11-a)for h in R(11-b) if all(all(r[b:b+h])for r in g[a:a+k])):
-  for i in R(a+1,c-1):
-   for j in R(b+1,d-1):g[i][j]-=(g[i][j]>2)*N
-  N=3
+def p(g,R=range,N=0):
+ for f,a,b,k,h in sorted((-all(all(r[b:b+h])for r in g[a:a+k])*k*h,a,b,k,h)for a in R(9)for b in R(9)for k in R(11-a)for h in R(11-b)):
+  for r in g[a+1:a+~-k]*-f:
+   for j in R(b+1,b+~-h):r[j]=2-(r[j]!=2)*N
+  N=1
  return g
