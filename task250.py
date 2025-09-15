af3c0bf9ec1d9eaecf2f54bc80b6307a29b8cf78
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-15 18:01:18 +0200

    250 -25

--- task250.py
+++ task250.py
@@ -1,6 +1,7 @@
-def p(g,R=range(10)):
- for _ in'    ':
-  for r in(g:=[*map(list,zip(*g[::-1]))])[:(a:=[i for i in R for j in R if g[i][j]==2][0])]:
-   for j in R:
+def p(g):
+ for _ in" "*4:
+  a=sum(g:=[*map(list,zip(*g[::-1]))],[]).index(2)//10
+  for r in g[:a]:
+   for j in range(10):
     if r[j]:r[j]=0;g[a-1][j]=5
  return g
