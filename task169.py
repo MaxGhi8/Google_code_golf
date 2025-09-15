1316b382f88f2e56b0ef387ed85608981a548eff
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-15 22:44:19 +0200

    169 -4

--- task169.py
+++ task169.py
@@ -1,7 +1,3 @@
 def p(g,R=range(10)):
- def f(i,j,m):
-  k=1
-  for x,y in(1,0),(-1,0),(0,1),(0,-1):
-   if 10>(x:=i+x)>-1<(y:=j+y)<10and m[x][y]>4:m[x][y]=-1;k+=f(x,y,m)
-  return k
+ f=lambda i,j,m:1+sum(m[a].__setitem__(b,-1)or f(a,b,m)for x,y in zip((1,-1,0,0),(0,0,1,-1))if 10>(a:=i+x)>-1<(b:=j+y)<10and m[a][b]>4)
  return[[g[i][j]and 6-f(i,j,[*map(list,g)])for j in R]for i in R]
