c44e67eecd0b756f93c709d5f1ff3e491d12e2c0
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-09-17 19:57:00 +0200

    361 -17

--- task361.py
+++ task361.py
@@ -1,3 +1,1 @@
-def p(g,R=range(-9,9),z=[[0]*10],S=sum,Z=zip,M=max):
- for _ in" "*3:g=M((S(S(map(lambda w:0<w[0]==w[1],Z(*r)))for r in Z(g,K)),(m:=[[*map(M,Z(*r))]for r in Z(g,K)])[::(-1)**({*S(g,[])}=={0,3})],m)for K in[z*y+[[0]*x+[*r[M(-x,0):]]+[0]*9for r in[*Z(*g[::-1])][M(-y,0):]]+z*9for x in R for y in R])[2]
- return g
+p=lambda g,R=range(-9,9),z=[[0]*10],S=sum,Z=zip,M=max:[(g:=M((S(a==b>0 for r in Z(g,K)for a,b in Z(*r)),(m:=[[*map(M,*r)]for r in Z(g,K)])[::1-2*({*S(g,[])}=={0,3})],m)for K in[z*y+[[0]*x+[*r[M(-x,0):]]+[0]*9for r in[*Z(*g[::-1])][M(-y,0):]]+z*9for x in R for y in R])[2])for _ in' '*3][-1]
