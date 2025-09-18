42be10b6f443a947540371299e4a420194ef47eb
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-09-18 15:57:31 +0200

    325 -2

--- task325.py
+++ task325.py
@@ -5,5 +5,5 @@ def p(g,R=range):
   for x,y in(1,0),(-1,0),(0,1),(0,-1):
    if h>(x:=i+x)>-1<(y:=j+y)<w and g[x][y]:f(x,y)
  for i in R(h*w):
-  if g[i//w][i%w]:k+=1;f(i//w,i%w)
- return[[8*(i==j)for j in R(k)]for i in R(k)]
+  if g[a:=i//w][b:=i%w]:k+=1;f(a,b)
+ return[[0]*i+[8]+[0]*(k+~i)for i in R(k)]
