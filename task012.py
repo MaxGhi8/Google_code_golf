815c0015878fc21e4a184a39501abbc7d3e1dfeb
alessdal <alessandro.daldossi@gmail.com>
2025-09-19 23:54:41 +0200

    improvements

--- task012.py
+++ task012.py
@@ -1,4 +1,4 @@
 def p(g):
  m=[p[:]for p in g];k,h=sorted({*(s:=sum(g,[]))},key=s.count)[:2]
- for t in range(2500):r,c,I=t//250,t//25%10,t%25;m[r+I//5-2][c+I%5-2]|=(I in{2,10,14,22})*h*(v:=g[r][c]==k)or(I%2<1)*k*v
+ for I in range(2500):r,c=I//250,I//25%10;I%=25;m[r+I//5-2][c+I%5-2]|=(I in{2,10,14,22})*h*(v:=g[r][c]==k)or(I%2<1)*k*v
  return m
--- task015.py
+++ task015.py
@@ -1,3 +1,3 @@
 def p(g):
- for t in range(576):r,c,I=t//72,t//9%8,t%9;i,j=I//3-1,t%3-1;g[r+i][c+j]=I%2*7*((v:=g[r][c])==1)or g[r+i][c+j]or(I%2<1)*4*(v==2)
+ for I in range(576):r,c,j=I//72,I//9%8,I%3-1;I%=9;i=I//3-1;g[r+i][c+j]=I%2*7*((v:=g[r][c])==1)or g[r+i][c+j]or(I%2<1)*4*(v==2)
  return g
