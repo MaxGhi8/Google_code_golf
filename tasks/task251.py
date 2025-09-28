7c4904132ed4e570442f9c5554ad7795c22f0f11
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-09-28 22:44:48 +0200

    improve 251 -6

--- tasks/task251.py
+++ tasks/task251.py
@@ -1,4 +1,4 @@
 def p(g,E=enumerate):
- for r,R in E(g[1:-1],1):
-  for c,C in E(R[1:-1],1):R[c]|=C<1and g[r-1][c]*R[c-1]and 2in R[c:]and 2in[*zip(*g)][c][r:]
+ for r,R in E(g[1:],1):
+  for c,C in E(R[1:],1):R[c]|=C<1and g[r-1][c]*R[c-1]and 2in{*R[c:]}&{*[*zip(*g)][c][r:]}
  return g
