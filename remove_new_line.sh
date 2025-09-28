87f378ce1be889bf6c689127d6eacd1fdccd4bcb
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-09-28 09:01:31 +0200

    update remove_new_line to search in all our folders (hard-coded)

--- remove_new_line.sh
+++ remove_new_line.sh
@@ -1,3 +1,15 @@
 for f in $(ls tasks/*); do
 	 perl -0pi -e "s/\R*\z//g" $f
    done
+
+for f in $(ls for_compression/*); do
+	 perl -0pi -e "s/\R*\z//g" $f
+   done
+
+for f in $(ls finals/*); do
+	 perl -0pi -e "s/\R*\z//g" $f
+   done
+
+for f in $(ls compressed/*); do
+	 perl -0pi -e "s/\R*\z//g" $f
+   done
