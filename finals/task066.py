6603f8315904e6ad4f5eb69da8ade9c2f6011de7
Oxke <osea.fracchia@iusspavia.it>
2025-09-29 21:56:15 +0200

    66 -3

--- finals/task066.py
+++ finals/task066.py
@@ -4,5 +4,4 @@ def p(g):
   if(s:=s[:])[i]%3<1:s[i]=3;return f(s,i+d,d,N)
   if s[i]&8:i-=d;d=[1,L][d*d<2];return f(s,i+d,d,N+1)or f(s,i-d,-d,N+1)
   return~-N*s
- L=len(g);s=sum(g,[]);I=s.index(3);d=[L,1][s[I+1]&1]
- s=f(s,I,-d)*(s[I-d]!=8)or f(s,I,d);return[*zip(*[iter(s)]*L)]
+ s=sum(g,[]);I=s.index(3);d=[L:=len(g),1][s[I+1]&1];s=f(s,I,-d)*(s[I-d]<8)or f(s,I,d);return[*zip(*[iter(s)]*L)]
