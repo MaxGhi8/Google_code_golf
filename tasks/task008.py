bf58c2044cecb53c1593e1ea799abc1abf001cba
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-03 14:18:29 +0200

    8 -11

--- tasks/task008.py
+++ tasks/task008.py
@@ -1,5 +1,6 @@
 def p(g):
  for _ in[0]*4:
-  g=[*zip(*g)][::-1];D={v:i for i,r in enumerate(g)for v in[2,8]if v in r};a=D[8]
-  if a<D[2]and 2not in g[a]:g=(g[:a+1]+[r for r in g if 2in r]+[[0]*len(g[0])]*99)[:len(g)]
+  g=[*zip(*g)][::-1];L=[]
+  for i,r in enumerate(g):2in r and(L.append(r),b:=i);8in r and(a:=i)
+  a<b and 2not in g[a]and(g:=(g[:a+1]+L+[[0]*len(r)]*99)[:len(g)])
  return g
