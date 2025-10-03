89ca3c14600fd9666595039bf59f7375c0112c97
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-03 16:13:42 +0200

    update 145

--- for_compression/task145.py
+++ for_compression/task145.py
@@ -1,5 +1,5 @@
-def p(g,c=1,A=0):
- for a,*x,Y,h in sorted((-h*k,a,a+k,b,h)for a in range(len(g))for b in range(len(g[0]))for k in range(-a-~len(g))for h in range(-b-~len(g[0]))if all(sum(r[b:b+h])<1for r in g[a:a+k])):
-  if a>A:return p(g,c+2)
-  for i in range(*x):g[i][Y:Y+h]=[c]*h;A=a
- return[[{1:1,2:2,c-2:8}.get(v,0)for v in r]for r in g]
+def p(g,c=1,m=0):
+ for a,*u,Y,h in sorted((-h*k,a,a+k,b,h)for a in range(len(g))for b in range(len(g[0]))for k in range(-a-~len(g))for h in range(-b-~len(g[0]))if all(sum(r[b:b+h])<1for r in g[a:a+k])):
+  if a>m:return p(g,c+2)
+  for i in range(*u):g[i][Y:Y+h]=[c]*h;m=a
+ return[[{1:1,2:2,c-2:8}.get(h,0)for h in r]for r in g]
