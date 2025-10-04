d8b4884419066e5d625f8dfa543140d22b7797e9
aleghi8 <alessandro.ghiotto01@universitadipavia.it>
2025-10-04 14:10:15 +0200

    opt for compression 93 (still to optimize)

--- tasks/task093.py
+++ tasks/task093.py
@@ -1,8 +1,8 @@
-def p(g,E=enumerate):
- for i,j in{(i,j)for i,r in E(g)for j,x in E(r)if x==5}:
-  for a,b in(0,1),(0,-1),(1,0),(-1,0):
-   u=x=i+a;v=y=j+b
-   while-1<x<14 and-1<y<14 and g[x][y]-5:
-    if g[x][y]%5:g[u][v]=5;g[x][y]*=x^u|y^v<1;u+=a;v+=b
-    x+=a;y+=b
+def p(g,c=enumerate):
+ for i,f in{(i,f)for i,r in c(g)for f,j in c(r)if j==5}:
+  for a,r in(0,1),(0,-1),(1,0),(-1,0):
+   d=j=i+a;y=y=f+r
+   while-1<j<14 and-1<y<14 and g[j][y]-5:
+    if g[j][y]%5:g[d][y]=5;g[j][y]*=j^d|y^y<1;d+=a;y+=r
+    j+=a;y+=r
  return g
