51386a8d8fe9c596350bbcec5c62599fbb66a0bd
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-05 09:59:00 +0200

    restore selece_best_task

--- select_best_tasks.py
+++ select_best_tasks.py
@@ -27,9 +27,9 @@ from dataclasses import dataclass
 
 TASK_RANGE = range(1, 401)
 # Candidate source directories in preference order for tie-breaking when --force
-FILES_PER_DIR = ["tasks", "submission"]
+FILES_PER_DIR = ["tasks", "compressed"] # "submission"
 # Destination folder containing the chosen best versions
-FINALS_DIR = "tasks"
+FINALS_DIR = "finals"
 
 @dataclass
 class Candidate:
