bf994033683766c2bad25dfa282499c337aeca24
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-06 09:44:26 +0200

    improve 096 -30

--- for_compression/task096.py
+++ for_compression/task096.py
@@ -1,10 +1,6 @@
 import re
-def p(p):
- o=re.sub(r',\s','',str(p+[*zip(*p)]));o+=o[::-1];i=int(max(o,key=o.count));s={0:(0,i)}
- for j in{*range(10)}-{i}:
-  if(t:=re.findall(f'{j}+',o)):
-   n=len((re.findall(f'{j}{j}[^]){j}]+{j}',o)+[3*'0'])[0])-3
-   t=len(max(t,key=len))*(1,2)[n>0]
-   s[(t+n)//2]=((n+1)//2,j)
- r=2*max(s)+1
- return[[i if(t[0]<(t:=s[t[1]])[0])else t[1]for j in range(r)if(t:=sorted([abs(j-r//2),abs(e-r//2)]))]for e in range(r)]
+def p(i):
+ r=re.sub(', ','',str(i+[*zip(*i)]));i=int(max(r,key=r.count));l={0:(0,i)}
+ for t in range(10):
+  if(t!=i)*(e:=re.findall(f'{t}+',r+r[::-1])):d=len((re.findall(f'{t}{t}([^]){t}]+){t}|$',r+r[::-1]))[0]);l[len(max(e))*((d>0)+1)+d>>1]=d+1>>1,t
+ return[[i*((d:=l[r[1]])[0]>r[0])or d[1]for t in range(-max(l),max(l)+1)if(r:=sorted((abs(t),abs(e))))]for e in range(-max(l),max(l)+1)]
--- tasks/task096.py
+++ tasks/task096.py
@@ -1,10 +1,6 @@
 import re
-def p(g,R=range,f=re.findall):
- t=re.sub(r',\s','',str(g+[*zip(*g)]));t+=t[::-1];b=int(max(t,key=t.count));B={0:(0,b)}
- for c in{*R(10)}-{b}:
-  if(v:=f(f'{c}+',t)):
-   m=len((f(f'{c}{c}[^]){c}]+{c}',t)+[3*'0'])[0])-3
-   l=len(max(v,key=len))*(1,2)[m>0]
-   B[(l+m)//2]=((m+1)//2,c)
- x=2*max(B)+1
- return[[b if(z[0]<(p:=B[z[1]])[0])else p[1]for j in R(x)if(z:=sorted([abs(j-x//2),abs(i-x//2)]))]for i in R(x)]
+def p(i):
+ r=re.sub(', ','',str(i+[*zip(*i)]));i=int(max(r,key=r.count));l={0:(0,i)}
+ for t in range(10):
+  if(t!=i)*(e:=re.findall(f'{t}+',r+r[::-1])):d=len((re.findall(f'{t}{t}([^]){t}]+){t}|$',r+r[::-1]))[0]);l[len(max(e))*((d>0)+1)+d>>1]=d+1>>1,t
+ return[[i*((d:=l[r[1]])[0]>r[0])or d[1]for t in range(-max(l),max(l)+1)if(r:=sorted((abs(t),abs(e))))]for e in range(-max(l),max(l)+1)]
