a09623071e70888efff02d8d9e8eaf0479685229
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-07 17:16:20 +0200

    344 -88 REPLACE for easy task is even better than regex

--- tasks/task344.py
+++ tasks/task344.py
@@ -1,8 +1,1 @@
-def p(g,e=enumerate):
- for i,r in e(g):
-  for j,v in e(r):
-   for x,y in(i+1,j),(i-1,j),(i,j+1),(i,j-1):
-    try:
-     if 3==v==g[x][y]+1and-1<x|y:r[j]=8;g[x][y]=0
-    except:0
- return g
+p=lambda g:exec("g[:]=[[*map(int,str(r)[1::3].replace('32','80'))]for r in zip(*g[::-1])];"*4)or g
