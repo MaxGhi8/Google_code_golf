0ec1c84370027f57f0fb0e5c8798c94d4b10633d
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-13 10:37:24 +0200

    2 -77

--- tasks/task002.py
+++ tasks/task002.py
@@ -1,7 +1,1 @@
-def p(g):
- def f(i,j):
-  try:
-   if g[i][j]<1:g[i][j]=1;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1)
-  except:0
- for i in range(len(g)):f(i,0);f(i,-1);f(0,i);f(-1,i)
- return[[(4,0,0,3)[a]for a in r]for r in g]
+p=lambda g,k=63:-k*[[v*(v>1)for v in r]for r in g]or p([[x%4or v==1or 4for x,v in zip(r,[1]+r)]for*r,in zip(*g[::-1])],k-1)
