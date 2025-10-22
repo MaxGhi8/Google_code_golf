bf5cc163ea2c585c36e44899d7acc1c5a0a0e28d
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2025-10-22 21:57:51 +0200

    77 -29

--- tasks/task077.py
+++ tasks/task077.py
@@ -1,3 +1,1 @@
-def p(g):
- for _ in' '*24:l=[0]*len(g);g=[[[2*(a==2)or 4*(b in{2,4})*(c in{2,4}or d in{2,4})or a,l:=r][0]for a,b,c,d in zip(r,[0]+r,l,r[1:]+[0])]for*r,in zip(*g[::-1])]
- return g
+import re;p=lambda g,k=23,x='(?<=[24]..)[13-9]':-k*g or p(eval(re.sub(f'(?<=[24].{{{len(g)*3+1}}}){x}|{x}(?=..[24])','4',str([*zip(*g)][::-1]))),k-1)
