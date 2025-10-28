10bea961c7c0ad1e636abf60cfd02f4442fce365
Oxke <osea.fracchia@iusspavia.it>
2025-10-29 00:14:30 +0100

    golf 302

--- tasks/task302.py
+++ tasks/task302.py
@@ -1,4 +1,1 @@
-def p(g):
- for s in 5,4,3:
-  for I in range(169):j=I%13;I//=13;M=g[I:I+s];sum(r[j:j+s].count(5)for r in M)-~-s*4 or [r.__setitem__(slice(j+1,j+~-s),[s+3]*(s-2))for r in M[1:-1]]
- return g
+p=lambda g,k=3:-k*g or p([r.count(5)>1and((i:=r.index(5)),(d:=~i+r.index(5,i+1)),r[:i+1]+(d+5,)*d+r[i+d+1:])[-1]or r for r in zip(*g[::-1])],k-1)
