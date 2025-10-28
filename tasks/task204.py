716497aada539f8ad1864f7073c9b8bd85756d7c
Oxke <osea.fracchia@iusspavia.it>
2025-10-28 22:42:14 +0100

    fix 204

--- tasks/task204.py
+++ tasks/task204.py
@@ -1,5 +1,1 @@
-def p(g):
- R=range;h,w=len(g),len(g[0])
- for i,j,l in[(i,j,l)for i in R(h)for j in R(w)for l in R(2,1+min(h-i,w-j))if all(g[i][j:j+l]+sum((g[i+k][j::~-l][:2]for k in R(l)),[])+g[i+~-l][j:j+l])]:
-  for r in g[i+1:i+~-l]:r[j+1:j+~-l]=[2|l%2*5]*(l-2)
- return g
+import re;p=lambda g,k=3:-k*g or p([eval(re.sub('1, (([^1], )*)1',lambda m:'1,'+f'{2+5*(len(m[1])%2)},'*(len(m[1])//3)+'1'if m[1]else m[0],str(r),count=1))for r in zip(*g[::-1])],k-1)
