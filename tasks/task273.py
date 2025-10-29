94df43a4b1c98b3de42c4343a2b44e18f2c2010b
Oxke <osea.fracchia@iusspavia.it>
2025-10-29 01:49:13 +0100

    273

--- tasks/task273.py
+++ tasks/task273.py
@@ -1,6 +1,6 @@
 def p(g):
  i=j=0
  for r in g:
-  if 4in r:i,j,*_=[(0,0),filter(r.__getitem__,range(10))][max(i,j)==0]
-  else:r[i+1:j]=[2]*(j+~i)
+  if 4in r:i,j=[(0,0),filter(r.__getitem__,range(10))][i==j]
+  else:r[-~i:j]=[2]*(j+~i)
  return g
